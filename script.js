c0027c8ce6df31189467a9799956fdf1be426685
Lucas Gabriel <lucas.gah2000@gmail.com>
2025-09-01 08:50:51 -0300

    usando outra api

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ async function buscarClima(){
     const cidade = document.getElementById("cidade");
     const resultado = document.getElementById("resultado");
 
-    if(!cidade){
+    if(!cidade[cidade]){
         resultado.innerHTML = "Digite uma cidade"
         return
     }
-    const apiKey = "b6907d289e10d714a6e88b30761fae22";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const cidades = {
+            "Sao Paulo": { lat: -23.55, lon: -46.63 },
+                "Rio de Janeiro": { lat: -22.91, lon: -43.17 },
+                    "Brasilia": { lat: -15.78, lon: -47.93 }
+                      };
+    }
+
+    const {lat, lon} = cidades[cidade]
+    const apiKey = "8NlspQzxfOTGL7Fa";
+    const url = `https://my.meteoblue.com/packages/basic-1h?apikey=${apiKey}&lat=${lat}&lon=${lon}&format=json`;
+    
 
     try {
         const resposta = await fetch(url);
@@ -15,10 +24,13 @@ async function buscarClima(){
 
         const dados = await resposta.json();
 
-        resultado.innerHTML = ` Temperatura em ${dados.name}:${dados.main.temp}°C`
+        const temperatura = dados.data_temperature2m[0]
+
+
+        resultado.innerHTML = ` Temperatura em ${cidade}:${temperatura}°C`
 
     } catch(error){
-        resultado.innerHTML = "Erro" + error.message
+        resultado.innerHTML = "Erro: " + error.message
     }
 
 
